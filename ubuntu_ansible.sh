589cf8a541b06429b85a6bda1c3f6409af314ad7
soewin <soewin.pku@gmail.com>
2020-06-01 05:12:03 +0000

    first commit

---
+++ ubuntu_ansible.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Installion ansible on ubuntu server
+
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update
+
+sudo apt install -y  ansible
+
+# adding host name
+
+cat >> /etc/hosts << EOF
+ayait1c.mylabserver.com
+ayait2c.mylabserver.com
+ayait3c.mylabserver.com
+EOF
+
+# Create password less sudo edit
+sudo touch /etc/sudoers.d/$USER
+cat > /etc/sudoers.d/$USER << EOF
+$USER ALL=(ALL)	NOPASSWD: ALL
+EOF
+
+
+
+
+
+
+
